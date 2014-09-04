f5932436857b76c1382a99fb8f0dfda246dc4d99
wangjeaf <zhifu.wzf@alibaba-inc.com>
2014-09-04 12:00:44 +0800

    remove ;

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ function loadCommands() {
     if (path.extname(filename) != '.js') {
       return
     }
-    var cmdMeta = require(cmdPath(filename));
-    var meta = cmdMeta.meta;
-    var handle = cmdMeta.handle;
+    var cmdMeta = require(cmdPath(filename))
+    var meta = cmdMeta.meta
+    var handle = cmdMeta.handle
     if (!meta || !handle) {
       console.error(filename + ' 文件，不符合ckstyle命令文件规范')
       console.error('每一个命令必须包含 meta(命令描述) 和 handle(命令处理函数) 两个部分')
-      return;
+      return
     }
-    var cmd = commander.command(meta.name);
+    var cmd = commander.command(meta.name)
     cmd.description(meta.description)
     if (meta.options) {
       meta.options.forEach(function(op) {
@@ -30,33 +30,34 @@ function loadCommands() {
       })
     }
     cmd.action(handle)
-  });
+  })
 }
 
 exports.command = function(args) {
-  loadCommands();
+  loadCommands()
 
   commander.parse(args)
 
   var rawArgs = commander.rawArgs
-  //[ 'node', '/usr/local/bin/ckstyle', 'demo' ]
+  // rawArgs示例：[ 'node', '/usr/local/bin/ckstyle', 'demo' ]
+
   if (rawArgs.length == 2) {
     commander.help()
   } else if (rawArgs.length > 2) {
-    var name = rawArgs[2];
+    var name = rawArgs[2]
 
     var found = commander.commands.some(function(cmd) {
-      return cmd._name == name;
+      return cmd._name == name
     })
 
     if (!found) {
       console.log('[CKStyle] Sorry, ckstyle can not find sub command: '.red + name.red)
-      return;
+      return
     }
   }
 }
 
-exports.CssParser = require('./ckstyle/parser/index').CssParser;
-exports.CssChecker = require('./ckstyle/ckstyler').CssChecker;
-exports.Detector = require('./ckstyle/browsers/Hacks');
+exports.CssParser = require('./ckstyle/parser/index').CssParser
+exports.CssChecker = require('./ckstyle/ckstyler').CssChecker
+exports.Detector = require('./ckstyle/browsers/Hacks')
 
