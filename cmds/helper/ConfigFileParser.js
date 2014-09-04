83752ac3e67b143f2ad8fb2237cb087d4b77a441
wangjeaf <zhifu.wzf@alibaba-inc.com>
2014-09-04 18:12:54 +0800

    add comments

--- cmds/helper/ConfigFileParser.js
+++ cmds/helper/ConfigFileParser.js
@@ -1,5 +1,8 @@
-exports.parse = function(file, originConfig) {
-    // 1. read file
+exports.loadConfig = function(file, originConfig) {
+    // 1. read file (ckstyle.json/ckstyle.ini/ckstyle-config.js)
+        // 1.1 file exists, read it
+        // 1.2 else read config from current dir
+        // 1.3 else read config from user dir
     // 2. parse it
     // 3. replace some config of originConfig
     return originConfig
