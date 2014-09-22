7a0f252ac01d0323730eda103354663ba2ef2b04
wangjeaf <zhifu.wzf@alibaba-inc.com>
2014-09-22 11:03:17 +0800

    compare

--- tools/compare/index.js
+++ tools/compare/index.js
@@ -5,20 +5,32 @@ var CleanCSS = require('clean-css')
 var YUI = require('cssmin')
 var CKStyle = require('../../index.js')
 
-var content = fs.readFileSync(path.join(__dirname, './test.css'), 'utf-8')
+function compare(file, desc) {
+    var content = fs.readFileSync(path.join(__dirname, './' + file), 'utf-8')
+    var start = + new Date
+    var yuied = YUI(content)
+    var time = + new Date - start
+    console.log((desc || file) + ' - YUI     : ' + yuied.length + ' in ' + time + 'ms')
 
-var yuied = YUI(content)
-console.log('YUI     : ' + yuied.length)
+    var start = + new Date
+    var cleaned = new CleanCSS().minify(content)
+    var time = + new Date - start
+    console.log((desc || file) + ' - CleanCSS: ' + cleaned.length + ' in ' + time + 'ms');
 
-var cleaned = new CleanCSS().minify(content)
-console.log('CleanCSS: ' + cleaned.length);
+    var start = + new Date
+    var cked = CKStyle.compressStr(content)
+    var time = + new Date - start
+    console.log((desc || file) + ' - CKStyle : ' + cked.length  + ' in ' + time + 'ms')
+}
 
-var cked = CKStyle.compressStr(content)
-console.log('CKStyle : ' + cked.length)
+// var cleaned = new CleanCSS().minify('a{color: black; font: white,black,white,black, Microsoft Yahei black, white ,sarif;}')
+// console.log('CleanCSS: ' + cleaned);
 
+// var a = '.a {font-family: "tahoma", "arial", "Microsoft Yahei"}'
+// console.log(new CleanCSS().minify(a))
 
-var cleaned = new CleanCSS().minify('a{color: black; font: white,black,white,black, Microsoft Yahei black, white ,sarif;}')
-console.log('CleanCSS: ' + cleaned);
-
-var a = '.a {font-family: "tahoma", "arial", "Microsoft Yahei"}'
-console.log(new CleanCSS().minify(a))
+compare('test.css')
+compare('normalize.css')
+compare('bootstrap.css')
+compare('animate.css')
+compare('foundation.css')
