e806d54f6bf3a64a320e0d998e715697b00611eb
wangjeaf <zhifu.wzf@alibaba-inc.com>
2014-09-29 17:21:35 +0800

    up

--- index.js
+++ index.js
@@ -67,3 +67,86 @@ exports.Detector = require('./ckstyle/browsers/Hacks')
 exports.compressStr = require('./ckstyle/doCssCompress').compressStr
 exports.fixStr = require('./ckstyle/doCssFix').fixStr
 exports.formatStr = require('./ckstyle/doCssFormat').formatStr
+
+var CKStyle = function(code, options) {
+    this._cache = {
+        plugins: {
+            before: [],
+            after: []
+        },
+        options: options,
+        code: code,
+        _compress: '',
+        _fix: '',
+        _check: '',
+        _format: ''
+    }
+}
+
+CKStyle.prototype = {
+    plugin: function(plugin, options) {
+        this._cache.plugins.before.push([plugin, options])
+        return this
+    },
+    _before: function() {
+        if (this._befored) {
+            return
+        }
+        this._befored = 1
+        var code = this._cache.code
+        var options = this._cache.options
+        var before = this._cache.plugins.before
+        before.forEach(function(b) {
+            code = b[0](code, b[1], options)
+        })
+        this.code = code
+    },
+    check: function(callback) {
+        return this
+    },
+    compress: function(callback) {
+        var options = this._cache.options
+        this._before()
+        var code = compressStr(this.code, options)
+        this._compress = this._latest = code
+        callback && callback(code)
+        return this
+    },
+    format: function(callback) {
+        var options = this._cache.options
+        this._before()
+        var code = formatStr(this.code, options)
+        this._format = this._latest =  code
+        callback && callback(code)
+        return this
+    },
+    fix: function(callback) {
+        var options = this._cache.options
+        this._before()
+        var code = fixStr(this.code, options)
+        this._fix = this._latest =  code
+        callback && callback(code)
+        return this
+    },
+    output: function(type) {
+        if (!type) {
+            return this._latest
+        }
+        if (typeof type == 'function') {
+            type(this._latest)
+            return this._latest
+        } else {
+            return this['_' + type]
+        }
+    }
+}
+
+CKStyle.start = function(css, options) {
+    return new CKStyle(css, options)
+}
+
+CKStyle.compress = exports.compressStr
+CKStyle.format = exports.formatStr
+CKStyle.fix = exports.fixStr
+
+exports.CKStyle = CKStyle
